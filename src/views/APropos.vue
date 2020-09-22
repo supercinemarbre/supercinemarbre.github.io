c644d1736cab7d3d9ea7e94f1b8bf587fe68d7e8
Marwane Kalam-Alami <marwane@kalam-alami.net>
2020-09-22 10:57:39 +0200

    Add call to timestamp contributions, timestamps for episodes 22-37

--- src/views/APropos.vue
+++ src/views/APropos.vue
@@ -5,19 +5,29 @@
     <p><a href="https://www.supercinebattle.fr/">Super Ciné Battle</a> est un podcast de Daniel Andreyev et Stéphane Bouley, visant à établir le classement ultime du cinéma.</p>
 
     <p>Super Ciné Marbre n'est pas affilié à Super Ciné Battle, et ne vise qu'à diffuser humblement le savoir du marbre au plus grand nombre.</p>
-    
-    <p>En cas d'erreurs ou de suggestions, vous pouvez me contacter à <a href="https://twitter.com/mkalamalami">@mkalamalami</a> / <code>Wan#2191</code>. Si vous savez utiliser Github c'est encore mieux : <a href="https://github.com/supercinemarbre/supercinemarbre.github.io">le projet est open source</a> et sous licence libre, vos contributions y sont les bienvenues.</p>
 
-    <h2>Données</h2>
+    <h2>Me contacter</h2>
+    
+    <p>Pour remonter des erreurs, contribuer des timestamps ou des suggestions de fonctionnalités, vous pouvez me contacter sur :
+    <ul>
+      <li>Twitter: <a href="https://twitter.com/mkalamalami">@mkalamalami</a></li>
+      <li>Discord: <code>Wan#2191</code></li>
+    </ul>
+    <p>Si vous savez utiliser Github c'est encore mieux : <a href="https://github.com/supercinemarbre/supercinemarbre.github.io">le projet est open source</a> et sous licence libre, vos contributions y sont les bienvenues.</p>
+
+    <h2>Licences</h2>
+
+    <h3>Données</h3>
 
     <ul>
       <li>Utilise des données de <a href="http://www.omdbapi.com/">OMDB</a> sous licence CC BY-NC 4.0.</li>
       <li>Information courtesy of IMDb (http://www.imdb.com). Used with permission.</li>
       <li>Remerciements à Quentin Haroche pour les timestamps film par film des épisodes 0 à 60 (en cours d'intégration).</li>
+      <li>Remerciements pour timestamps additionnels : Syta, walydbecharef</li>
       <li>Sans oublier les listes de <a href="http://supercinebattle.fr/">Super Ciné Battle</a>.</li>
     </ul>
 
-    <h2>Images</h2>
+    <h3>Images</h3>
 
     <ul>
       <li><a href="https://icons8.com/icons/set/imdb">IMDb icon</a> icon by <a href="https://icons8.com">Icons8</a></li>
